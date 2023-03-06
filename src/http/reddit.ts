3d160686fc13795e2bbe8e30f7e2edf7946cf373
Tianxu Zhang <tiacial0114@gmail.com>
2023-03-05 18:38:40 -0800

    Addressing reddit cors issue

--- src/http/reddit.ts
+++ src/http/reddit.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import querystring from 'querystring';
 
+const corsHeaders = {
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': '*',
+};
+
 export const getRedditToken = async (queryCode: string) => {
   const payload = {
     grant_type: 'authorization_code',
@@ -15,6 +21,7 @@ export const getRedditToken = async (queryCode: string) => {
         Authorization: `Basic ${btoa(
           `${import.meta.env.VITE_REDDIT_CLIENT_ID}:${import.meta.env.VITE_REDDIT_CLIENT_SECRET}`,
         )}`,
+        ...corsHeaders,
         'content-type': 'application/x-www-form-urlencoded',
       },
     },
@@ -26,6 +33,7 @@ export const getRedditUser = async (token: string) => {
   const data = await axios.get('https://oauth.reddit.com/api/v1/me.json', {
     headers: {
       Authorization: `bearer ${token}`,
+      ...corsHeaders,
     },
   });
   return data.data;
